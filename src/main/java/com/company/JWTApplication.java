ce7418b555cfd5259353e1ca872760bb26f4e4d1
MuradAbbaszade <muradabbaszade6@gmail.com>
2022-09-19 12:18:55 +0500

    Update user and role controllers

--- src/main/java/com/company/JWTApplication.java
+++ src/main/java/com/company/JWTApplication.java
@@ -1,11 +1,37 @@
 package com.company;
 
+import com.company.configuration.WebSecurityConfig;
+import com.company.serviceImpl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class JWTApplication {
+    @Autowired
+    UserServiceImpl userService;
+    @Bean
+    public CommandLineRunner run() {
+        CommandLineRunner clr = new CommandLineRunner() {
+            @Override
+            public void run(String... args) {
+                String password = userService.getUserById(new Long(1)).getPassword();
+                String encodedPassword = passwordEncoder().encode(password);
+                userService.getUserById(new Long(1)).setPassword(encodedPassword);
+            }
+
+        };
+        return clr;
+    }
     public static void main(String[] args){
         SpringApplication.run(JWTApplication.class, args);
+    }
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/company/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/company/serviceImpl/UserServiceImpl.java
@@ -2,22 +2,30 @@ package com.company.serviceImpl;
 
 import com.company.entity.Role;
 import com.company.entity.User;
+import com.company.repo.RoleRepository;
 import com.company.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import com.company.service.UserService;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.graalvm.compiler.phases.common.DeadCodeEliminationPhase.Optionality.Optional;
 
 @Service
 @Transactional
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, UserDetailsService {
 
     @Autowired
     UserRepository userRepository;
+    RoleRepository roleRepository;
+
     @Override
     public User getUserByEmail(String email) {
         return userRepository.getUserByEmail(email);
@@ -59,5 +67,18 @@ public class UserServiceImpl implements UserService {
     @Override
     public void addRoleToUser(Long roleId, Long userId) {
         userRepository.addRoleToUser(roleId,userId);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = getUserByEmail(username);
+        if(user==null){
+            throw new UsernameNotFoundException("User not found");
+        }
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        List<Long> idsOfUserRoles = getIdsOfUserRoles(user.getId());
+        idsOfUserRoles.forEach(id->authorities.add(
+                new SimpleGrantedAuthority(roleRepository.findById(id).get().getName())));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(),user.getPassword(),authorities);
     }
 }
